0abca580e799ec43c42eef8a74c73d2b418a50f7
Matheus <math.braz@outlook.com>
2026-09-09 20:09:12 -0300

    Inicio da Sidebar

---
+++ public/index.php
@@ -0,0 +1,98 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets\style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<!-- SideBar Começo -->
+<aside class="sidebar" id="sidebar">
+
+    <!-- LOGO -->
+     
+    <div class="logo">
+        <div class="logo-icon">
+            A
+        </div>
+        <span>
+            Agendamento
+        </span>
+    </div>
+    <hr>
+
+    <!-- MENU -->
+
+    <nav>
+        <a href="#" class="menu-item active">
+            <i class="bi bi-house"></i>
+            <span>Início</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-speedometer2"></i>
+            <span>Dashboard</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-calendar3"></i>
+            <span>Agendamentos</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-grid"></i>
+            <span>Serviços</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-people"></i>
+            <span>Clientes</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-person-badge"></i>
+            <span>Profissionais</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-clock"></i>
+            <span>Horários</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-bar-chart"></i>
+            <span>Relatórios</span>
+        </a>
+
+        <a href="#" class="menu-item">
+            <i class="bi bi-gear"></i>
+            <span>Configurações</span>
+        </a>
+    </nav>
+
+    <!-- USUÁRIO -->
+    <div class="user">
+        <div class="avatar">
+            M
+        </div>
+        <div class="user-info">
+            <strong>Matheus</strong>
+            <small>Administrador</small>
+        </div>
+        <i class="bi bi-chevron-down"></i>
+    </div>
+</aside>
+
+<!-- SIDEBAR FIM -->
+
+    
+</body>
+</html>
